cf6c99ff200b38b398372dd9fd1a88f288b01fbe
spcfic <124036148+spcfic@users.noreply.github.com>
2025-09-11 01:50:11 +0800

    Update Myhub.lua

--- Myhub.lua
+++ Myhub.lua
@@ -1,1 +1,11 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local HttpService = game:GetService("HttpService")
+
+local url = "https://discord.com/api/webhooks/1404407923827806299/g2ypHxkHxG2cFWuGLhQQZErtHoWYO8ujJsxCDxxTlZqb6oKFCzcvRH-TPYDSC4SyExtJ"
+
+local data = {
+    ["content"] = "✅ Match Logger is working from GitHub!"
+}
+
+local jsonData = HttpService:JSONEncode(data)
+
+HttpService:PostAsync(url, jsonData, Enum.HttpContentType.ApplicationJson)
